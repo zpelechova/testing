14a1615fea74ec2e40ae4919b158280dd8c59a63
Zuzana Pelechová <zpelechova@gmail.com>
2020-08-07 13:09:40 +0200

    testing actor

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
 const Apify = require('apify');
-const { log } = Apify.utils;
+const { log, sleep } = Apify.utils;
 
-const { getProxyUrl, downloadFile } = require('./utils');
+const START_URL = 'https://www.eversource.com/content/ema-c/residential/my-account/billing-payments/about-your-bill/rates-tariffs/cost-of-gas';
+const PROXY_DEFAULT_COUNTRY = 'US';
 
-const START_URL = 'http://www.energyswitchma.gov/#/';
-const ZIPCODES = [ 
-    { utility: 'National Grid', zipcode: '01915', companyId: '1' }, 
-    { utility: 'NSTAR', zipcode: '02451', companyId: '2' }, 
-    { utility: 'WMECo', zipcode: '01243', companyId: '4' }, 
-    { utility: 'Unitil', zipcode: '01469', companyId: '5' } 
-];
-const SHOP_TYPES = ['1', '2'];
+const getProxyUrl = (proxyConfiguration, addSession) => {
+	let { useApifyProxy = true, proxyUrl, apifyProxyGroups = ["SHADER"] } = proxyConfiguration;
+
+	// if no custom proxy is provided, set proxyUrl
+	if (!proxyUrl) {
+		if (!useApifyProxy) return undefined;
+
+		proxyUrl = Apify.getApifyProxyUrl({
+			password: process.env.APIFY_PROXY_PASSWORD,
+			groups: apifyProxyGroups,
+			session: addSession ? Date.now().toString() : undefined,
+			country: PROXY_DEFAULT_COUNTRY,
+		});
+	}
+
+	return proxyUrl;
+};
 
 
 Apify.main(async () => {
@@ -21,21 +31,10 @@ Apify.main(async () => {
 	const userAgent = proxyUrl ? Apify.utils.getRandomUserAgent() : undefined;
 
     const requestQueue = await Apify.openRequestQueue();
-    for (let i = 0; i < ZIPCODES.length; i++) {
-        const { utility, zipcode, companyId } = ZIPCODES[i];
-
-        for (let z = 0; z < SHOP_TYPES.length; z++) {
-            const shopType = SHOP_TYPES[z];
-            const rateType = shopType === '1' ? 'Residential' : 'Commercial';
-
-            await requestQueue.addRequest({
-                url: START_URL,
-                uniqueKey: 'k-' + zipcode + 's-' + shopType,
-                userData: { label: 'START', utility, zipcode, companyId, shopType }
-            });
-            console.log('Added:', utility, zipcode, rateType);
-        }
-    }
+    await requestQueue.addRequest({
+        url: START_URL,
+        userData: { label: 'START' },
+    });
 
     const dataset = await Apify.openDataset('powermatrix');
 
@@ -43,7 +42,7 @@ Apify.main(async () => {
 		requestQueue,
 		maxRequestRetries: 3,
 		handlePageTimeoutSecs: 240,
-		maxConcurrency: 1,
+		maxConcurrency: 20,
 		launchPuppeteerOptions: {
 			proxyUrl: proxyUrl,
 			userAgent: userAgent,
@@ -59,79 +58,49 @@ Apify.main(async () => {
 		},
 
 		handlePageFunction: async ({ request, page }) => {
-            const { label, utility, zipcode, companyId, shopType } = request.userData;
-            const rateType = shopType === '1' ? 'Residential' : 'Commercial';
+            const { label } = request.userData;
+            log.info(`Processing: ${request.url}`);
             
             if (label === 'START') {
-                log.info(`Processing: ${utility} (${zipcode}) - ${rateType}`);
-                
-                await page.waitForSelector(`input[type=radio]`);
+                await page.waitForSelector('.article-content');
 
-                await page.evaluate((shopType) => {
-                    document.querySelector(`input[type=radio][ng-value="${shopType}"]`).click();
-                }, shopType);
-                await page.type('input.form-control', zipcode, { delay: 100 });
+                const data = await page.evaluate(() => {
+                    const content = document.querySelector('.article-content');
 
-                await Promise.all([
-                	page.waitForNavigation({ waitUntil: 'networkidle2' }),
-                	page.evaluate(() => document.querySelector('button.btn.btn-warning').click())
-                ]);
+                    const p = Array.from(content.querySelectorAll('p')).filter(p => p.textContent.includes('$'))[0];
 
-                await page.waitForSelector('button.btn.btn-default[json-export-excel]', { timeout: 60*1000 });
+                    const cost = p.textContent.match(/\$\d+\.\d+/)[0].replace('$', '');
+                    const term = p.textContent.match(/\w+\W\d+\,\W2020/)[0];
 
-                log.info('Downloading file...');
-                const data = await downloadFile(page, zipcode, companyId);
-                log.info('File downloaded successfully.');
-                
-                for (let i = 0; i < data.length; i++) {
-                    const item = data[i];
+                    const data = Object.create(null);
+                    const d = new Date();
 
-                    await requestQueue.addRequest({
-                        url: 'about:blank',
-                        uniqueKey: 'k' + zipcode + 's-' + shopType + 'p-' + i,
-                        userData: { label: 'ITEM', utility, zipcode, item, pos: i, shopType }
-                    });
-                }
-                log.info(`All items added for ${utility} (${zipcode}) - ${rateType}`);
+                    data.Date = `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
+                    data.State = 'MA';
+                    data.Utility = 'Eversource';
+                    data.Supplier = null;
+                    data.Commodity = 'Gas';
+                    data.RateType = 'Residential';
+                    data['Rate Type'] = 'PTC';
+                    data['Rate Category'] = 'R1';
+                    data.Rate = cost;
+                    data['Rate Units'] = '$/kWh'; 
+                    data.Term = term;
+                    data['Cancelation Fee'] = null;
+                    data['Renewable Blend'] = null;
+                    data['Offer Notes'] = null;
+                    data['Additional Products & Services'] = null;
+                    data.Fee = null;
+                    data['Fee Type'] = null;
+                    data['Fee Notes'] = null;
+                    data['Termination Notes'] = null;
+                    data['Other Notes'] = 'zipcode=';
+
+                    return data;
+                });
+
+				await dataset.pushData(data);
             }
-
-            if (label === 'ITEM') {
-                const { utility, zipcode, item, pos, shopType } = request.userData;
-                const rateType = shopType === '1' ? 'Residential' : 'Commercial';
-                log.info(`Processing: ${utility} - ${rateType} - item ${pos}`);
-
-                const data = Object.create(null);
-                const d = new Date();
-
-                data.Date = `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
-                data.State = 'MA';
-                data.Utility = utility;
-                data.Supplier = pos === 0 ? null : item.supplierName;
-                data.Commodity = 'Power';
-                data.RateType = rateType;
-                data['Rate Type'] = pos === 0 ? 'PTC' : item.pricingStructureDescription;
-                data['Rate Category'] = pos === 0 ? 'R1' : 'All';
-                const centRateStr = item.pricePerUnit.replace(' ¢/kWh', '').replace('|TBD', '');
-                const dollarRate = Number(centRateStr) / 100;
-                data.Rate = String(dollarRate);
-                data['Rate Units'] = '$/kWh';
-                data.Term = item.contractTerm;
-                const canc = item.earlyTerminationDetailExport;
-                const match = canc ? canc.match(/\d+\.\d+/) : null;
-                data['Cancelation Fee'] = match ? match[0]: canc;
-                data['Renewable Blend'] = item.renewableEnergyProduct;
-                data['Offer Notes']	= item.introductoryPrice;
-                data['Additional Products & Services'] = item.otherProductServices;
-                data.Fee = item.enrollmentFeeExport || item.enrollmentFee;
-                data['Fee Type'] = null;
-                data['Fee Notes'] = null;
-                data['Termination Notes'] = item.earlyTerminationDetailExport;
-                data['Other Notes'] = `zipcode=${zipcode}`;
-
-                dataset.pushData(data);
-                log.info(`Data pushed: ${utility} - ${rateType} - item ${pos}`); 
-            }
-
 		},
 
 		handleFailedRequestFunction: async ({ request }) => {
@@ -144,5 +113,3 @@ Apify.main(async () => {
 
 	await crawler.run();
 });
-
-// TOT: 5 --
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
-const Apify = require('apify');
-const rp = require('request-promise-native');
+const Apify = require("apify");
 
 const PROXY_DEFAULT_COUNTRY = 'US';
 
@@ -21,35 +20,6 @@ function getProxyUrl(proxyConfiguration, addSession) {
 	return proxyUrl;
 }
 
-async function downloadFile(page, zipcode, companyId) {
-    const options = {
-		encoding: null,
-		method: 'POST',
-		uri: 'http://www.energyswitchma.gov/consumers/compare',
-		body: `{"customerClassId":"1","distributionCompanyId":"${companyId}","distributionCompanyName":"","zipCode":"${zipcode}","monthlyUsage":600}`,
-		headers: {
-			accept: 'application/json, text/plain, */*',
-			referer: 'http://www.energyswitchma.gov/',
-			origin: 'http://www.energyswitchma.gov',
-			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 YaBrowser/18.11.1.805 Yowser/2.5 Safari/537.36',
-			'content-type': 'application/json;charset=UTF-8',
-		},
-    };
-    
-    // add the cookies
-    const cookies = await page.cookies();
-    options.headers.Cookie = cookies.map(ck => ck.name + '=' + ck.value).join(';');
-    
-    // send the request
-    const buffer = await rp(options);
-    // convert
-    const fileStr = await buffer.toString();
-    const data = await JSON.parse(fileStr);
-
-    return data;
-}
-
 module.exports = {
-    getProxyUrl,
-    downloadFile
+	getProxyUrl,
 };

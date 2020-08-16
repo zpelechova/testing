4dad4aa47683d02e8394d7e1dab2c11fa5c1a5d9
Zuzana Pelechová <zpelechova@gmail.com>
2020-08-16 12:55:16 +0200

    new

--- src/main.js
+++ src/main.js
@@ -1,86 +1,149 @@
-/**
- * This template is a production ready boilerplate for developing with `CheerioCrawler`.
- * Use this to bootstrap your projects using the most up-to-date code.
- * If you're looking for examples or want to learn more, see README.
- */
-
 const Apify = require('apify');
-const { handleBase, handleDetail } = require('./routes');
+const { log } = Apify.utils;
 
-const { utils: { log } } = Apify;
+const { getProxyUrl, downloadFile, objToCsv } = require('./utils');
+
+const START_URL = 'http://www.energyswitchma.gov/#/';
+const ZIPCODES = [
+    { utility: 'National Grid', zipcode: '01915', companyId: '1' },
+    { utility: 'NSTAR', zipcode: '02451', companyId: '2' },
+    { utility: 'WMECo', zipcode: '01243', companyId: '4' },
+    { utility: 'Unitil', zipcode: '01469', companyId: '5' }
+];
+const SHOP_TYPES = ['1', '2'];
+
 
 Apify.main(async () => {
-    const  startUrls = [
-        {
-          "url": "https://www.icc.illinois.gov/ags/products.aspx?said=1&mid=S",
-          "userData": {
-            "label": "BASE"
-          }
-        },
-        {
-          "url": "https://www.icc.illinois.gov/ags/products.aspx?said=2&mid=S",
-          "userData": {
-            "label": "BASE"
-          }
-        },
-        {
-          "url": "https://www.icc.illinois.gov/ags/products.aspx?said=3&mid=S",
-          "userData": {
-            "label": "BASE"
-          }
-        },
-        {
-          "url": "https://www.icc.illinois.gov/ags/products.aspx?said=1&mid=R",
-          "userData": {
-            "label": "BASE"
-          }
-        },
-        {
-          "url": "https://www.icc.illinois.gov/ags/products.aspx?said=2&mid=R",
-          "userData": {
-            "label": "BASE"
-          }
-        },
-        {
-          "url": "https://www.icc.illinois.gov/ags/products.aspx?said=3&mid=R",
-          "userData": {
-            "label": "BASE"
-          }
-        }
-      ]
+	const input = await Apify.getInput();
+	const { proxyConfiguration = {} } = input;
 
-    const requestList = await Apify.openRequestList('start-urls', startUrls);
+	const proxyUrl = getProxyUrl(proxyConfiguration, true);
+	const userAgent = proxyUrl ? Apify.utils.getRandomUserAgent() : undefined;
+
     const requestQueue = await Apify.openRequestQueue();
+    for (let i = 0; i < ZIPCODES.length; i++) {
+        const { utility, zipcode, companyId } = ZIPCODES[i];
 
-    const crawler = new Apify.CheerioCrawler({
-        requestList,
-        requestQueue,
-        useApifyProxy: false,
-        useSessionPool: true,
-        persistCookiesPerSession: true,
-        // Be nice to the websites.
-        // Remove to unleash full power.
-        maxConcurrency: 50,
-        // You can remove this if you won't
-        // be scraping any JSON endpoints.
-        additionalMimeTypes: [
-            'application/json',
-        ],
-        handlePageFunction: async (context) => {
-            const { url, userData: { label } } = context.request;
-            log.info('Page opened.', { label, url });
-            switch (label) {
-                case 'BASE':
-                    return handleBase(context, requestQueue, url);
-                case 'DETAIL':
-                    return handleDetail(context);
-                default:
-                    throw new Error("Unexpected label");
+        for (let z = 0; z < SHOP_TYPES.length; z++) {
+            const shopType = SHOP_TYPES[z];
+            const rateType = shopType === '1' ? 'Residential' : 'Commercial';
+
+            await requestQueue.addRequest({
+                url: START_URL,
+                uniqueKey: 'k-' + zipcode + 's-' + shopType,
+                userData: { label: 'START', utility, zipcode, companyId, shopType }
+            });
+            console.log('Added:', utility, zipcode, rateType);
+        }
+    }
+
+    const dataset = await Apify.openDataset('powermatrix');
+    await dataset.drop();
+
+    const crawler = new Apify.PuppeteerCrawler({
+		requestQueue,
+		maxRequestRetries: 3,
+		handlePageTimeoutSecs: 240,
+		maxConcurrency: 1,
+		launchPuppeteerOptions: {
+		    useApifyProxy: false,
+			userAgent: userAgent,
+			timeout: 120 * 1000,
+			headless: true,
+		},
+
+		gotoFunction: async ({ request, page }) => {
+			return page.goto(request.url, {
+				timeout: 180 * 1000,
+				waitUntil: 'networkidle2',
+			});
+		},
+
+		handlePageFunction: async ({ request, page }) => {
+            const { label, utility, zipcode, companyId, shopType } = request.userData;
+            const rateType = shopType === '1' ? 'Residential' : 'Commercial';
+
+            if (label === 'START') {
+                log.info(`Processing: ${utility} (${zipcode}) - ${rateType}`);
+
+                await page.waitForSelector(`input[type=radio]`);
+
+                await page.evaluate((shopType) => {
+                    document.querySelector(`input[type=radio][ng-value="${shopType}"]`).click();
+                }, shopType);
+                await page.type('input.form-control', zipcode, { delay: 100 });
+
+                await Promise.all([
+                	page.waitForNavigation({ waitUntil: 'networkidle2' }),
+                	page.evaluate(() => document.querySelector('button.btn.btn-warning').click())
+                ]);
+
+                await page.waitForSelector('button.btn.btn-default[json-export-excel]', { timeout: 60*1000 });
+
+                log.info('Downloading file...');
+                const data = await downloadFile(page, zipcode, companyId);
+                log.info('File downloaded successfully.');
+
+                for (let i = 0; i < data.length; i++) {
+                    const item = data[i];
+                    const csv = objToCsv(item);
+
+                    await requestQueue.addRequest({
+                        url: 'about:blank',
+                        uniqueKey: 'k' + zipcode + 's-' + shopType + 'p-' + i,
+                        userData: { label: 'ITEM', utility, zipcode, item, pos: i, shopType, csv }
+                    });
+                }
+                log.info(`All items added for ${utility} (${zipcode}) - ${rateType}`);
             }
-        },
-    });
 
-    log.info('Starting the crawl.');
-    await crawler.run();
-    log.info('Crawl finished.');
+            if (label === 'ITEM') {
+                const { utility, zipcode, item, pos, shopType, csv } = request.userData;
+                const rateType = shopType === '1' ? 'Residential' : 'Commercial';
+                log.info(`Processing: ${utility} - ${rateType} - item ${pos}`);
+
+                const data = Object.create(null);
+                const d = new Date();
+
+                data.Date = `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
+                data.State = 'MA';
+                data.Utility = utility;
+                data.Supplier = pos === 0 ? null : item.supplierName;
+                data.Commodity = 'Power';
+                data.RateType = rateType;
+                data['Rate Type'] = pos === 0 ? 'PTC' : item.pricingStructureDescription;
+                data['Rate Category'] = pos === 0 ? 'R1' : 'All';
+                const centRateStr = item.pricePerUnit.replace(' ¢/kWh', '').replace('|TBD', '');
+                data.Rate = Number(centRateStr) / 100;
+                data['Rate Units'] = '$/kWh';
+                data.Term = parseInt(item.contractTerm.replace(' months', ''));
+                const canc = item.earlyTerminationDetailExport;
+                const match = canc ? canc.match(/\d+\.\d+/) : null;
+                data['Cancellation Fee'] = match ? parseFloat(match[0].replace('$', '')) : canc;
+                data['Renewable Blend'] = item.renewableEnergyProduct ? parseInt(item.renewableEnergyProduct.replace('%', '')) : 0;
+                data['Offer Notes']	= item.introductoryPrice;
+                data['Additional Products & Services'] = item.otherProductServices;
+                data.Fee = item.enrollmentFeeExport || item.enrollmentFee;
+                data['Fee Type'] = null;
+                data['Fee Notes'] = null;
+                data['Termination Notes'] = item.earlyTerminationDetailExport;
+                data['Other Notes'] = `zipcode=${zipcode}`;
+                data['Csv copy'] = csv;
+
+                await dataset.pushData(data);
+                await Apify.pushData(data);
+                log.info(`Data pushed: ${utility} - ${rateType} - item ${pos}`);
+            }
+
+		},
+
+		handleFailedRequestFunction: async ({ request }) => {
+			console.log(`Request ${request.url} failed too many times`);
+			await dataset.pushData({
+				'#debug': Apify.utils.createRequestDebugInfo(request),
+			});
+		},
+	});
+
+	await crawler.run();
 });

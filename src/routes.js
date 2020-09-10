9fa0b8a8374429b63697bcd770c56e457ccf3ef0
Zuzana Pelechová <zpelechova@gmail.com>
2020-09-10 17:03:07 +0200

    txu

--- src/routes.js
+++ src/routes.js
@@ -9,17 +9,21 @@ exports.handleBase = async ({ request, page }) => {
     await page.waitForSelector(SELECTORS.PLAN_AREA);
     let plans = await page.$$(SELECTORS.PLAN_AREA);
     var results = [];
+
+    var NUMERIC_REGEXP = /[-]{0,1}[\d]*[.]{0,1}[\d]+/g;
+    
     for (let plan of plans) {
         const vendor = await plan.$eval(SELECTORS.VENDOR, elem => elem.innerText);
         let term = await plan.$eval(SELECTORS.TERM, elem => elem.innerText);
         term = term.trim().split("\n").join(" ");
+        rate_dirty =  await plan.$eval(SELECTORS.RATE, elem => elem.innerText);
         let result = {
             Date: (new Date()).toLocaleDateString("ISO"),
             State: "TX",
             "Customer Type": "Residential",
             Utility: vendor,
             Supplier: vendor,
-            Rate: await plan.$eval(SELECTORS.RATE, elem => elem.innerText),
+            Rate: rate_dirty.match(NUMERIC_REGEXP),
             Term: term,
             "Cancellation Fee": await plan.$eval(SELECTORS.CANCELLATION_FEE, elem => elem.innerText)
         }

047d95a73d9d932077a8e4ba470fdb719b073529
Zuzana Pelechová <zpelechova@gmail.com>
2020-09-11 12:01:29 +0200

    tesco

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,11 @@ exports.handleStart = async ({ $ }) =>
 {
     const requestQueue = await Apify.openRequestQueue();
 
-    const links = $( "ul .menu-superdepartment a" ).map( function() {  return $(this).attr('href'); }).get();
+    const links = $( "ul.menu li a" ).map( function() {  return $(this).attr('href'); }).get();
 
     for (i in links)
     {   
-        links[i] = `https://potravinydomov.itesco.sk/${links[i].replace('?include-children=true', '/all')}`
+        links[i] = `https://potravinydomov.itesco.sk${links[i].replace('?include-children=true', '/all')}`
         await requestQueue.addRequest({
         url: links[i],
         userData: { label: 'DETAIL' }

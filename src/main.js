8ff6e47a332d4abba090fec018df1bb0b4f1e613
Zuzana Pelechová <zpelechova@gmail.com>
2020-09-29 15:55:16 +0200

    oh

--- src/main.js
+++ src/main.js
@@ -1,118 +1,68 @@
-// This is the main Node.js source code file of your actor.
-// It is referenced from the "scripts" section of the package.json file,
-// so that it can be started by running "npm start".
+/**
+ * This template is a production ready boilerplate for developing with `PuppeteerCrawler`.
+ * Use this to bootstrap your projects using the most up-to-date code.
+ * If you're looking for examples or want to learn more, see README.
+ */
 
-// Import Apify SDK. For more information, see https://sdk.apify.com/
 const Apify = require('apify');
-const request = require("request-promise");
+const { handleBase, handleUtility } = require('./routes');
 
-const SELECTOR = {
-    ELECTRIC: "offer-summary-popup > div > div > div.incumbent-utility-selector > div:nth-child(1) > button",
-    DATASET: "div.footer-container a.dataset",
-    ACTIVE_OFFERS: "div.offers table.table.dataset button.btn-activeoffers"
-};
+const { utils: { log } } = Apify;
+
+const { LABEL, BLOCK_RESOURCES } = require("./config");
 
 Apify.main(async () => {
-    // Get input of the actor (here only for demonstration purposes).
-    // If you'd like to have your input checked and have Apify display
-    // a user interface for it, add INPUT_SCHEMA.json file to your actor.
-    // For more information, see https://apify.com/docs/actor/input-schema
-    const input = {
-        "url": "http://documents.dps.ny.gov/PTC/zipcode/14646"
-    };
-    console.log('Input:');
-    console.dir(input);
+    const startUrls = [
+        {
+          "url": "http://energychoice.ohio.gov/ApplesToApplesCategory.aspx?Category=Electric",
+          "method": "GET",
+          "userData": {
+            "label": "BASE"
+          }
+        }
+      ]
 
-    if (!input || !input.url) throw new Error('Input must be a JSON object with the "url" field!');
+    const requestList = await Apify.openRequestList('start-urls', startUrls);
+    const requestQueue = await Apify.openRequestQueue();
 
-    const dataset = await Apify.openDataset('powermatrix');
-    const store = await Apify.openKeyValueStore();
-
-    console.log('Launching Puppeteer...');
-    const browser = await Apify.launchPuppeteer();
-
-    console.log(`Opening page ${input.url}...`);
-    const page = await browser.newPage();
-    await page.goto(input.url);
-    await page.waitForSelector("#acceptOverlay");
-    await page.click("#acceptOverlay");
-    await page.waitForSelector("#scrollDown");
-    await page.click("#scrollDown");
-    await page.waitFor(2000);
-    await page.click("#acceptOverlay");
-    await page.waitForSelector(SELECTOR.ELECTRIC);
-    await page.click(SELECTOR.ELECTRIC);
-    await page.waitForSelector(SELECTOR.DATASET);
-    await page.click(SELECTOR.DATASET);
-    await page.waitFor(2000);
-    await page.waitForSelector(SELECTOR.ACTIVE_OFFERS);
-    await page.setRequestInterception(true);
-    page.click(SELECTOR.ACTIVE_OFFERS);
-    const xRequest = await new Promise(resolve => {
-        page.on('request', interceptedRequest => {
-            interceptedRequest.abort();     //stop intercepting requests
-            resolve(interceptedRequest);
-        });
+    const crawler = new Apify.PuppeteerCrawler({
+        requestList,
+        requestQueue,
+        useSessionPool: true,
+        persistCookiesPerSession: true,
+        launchPuppeteerOptions: {
+            useApifyProxy: true,
+            // Chrome with stealth should work for most websites.
+            // If it doesn't, feel free to remove this.
+            useChrome: true,
+            stealth: true,
+        },
+        gotoFunction: async ({request, page}) => {
+            await Apify.utils.puppeteer.blockRequests(page, {
+                urlPatterns: [
+                    ...BLOCK_RESOURCES.analytics,
+                    ...BLOCK_RESOURCES.patterns
+                ]
+            });
+            return page.goto(request.url, {
+                waitUntil: "domcontentloaded"
+            });
+        },
+        handlePageFunction: async (context) => {
+            const { url, userData: { label } } = context.request;
+            log.info('Page opened.', { label, url });
+            switch (label) {
+                case LABEL.BASE:
+                    return handleBase(context, requestQueue);
+                case LABEL.UTILITY:
+                    return handleUtility(context);
+                default:
+                    throw new Error("Don't know what to do with this");
+            }
+        },
     });
 
-    const options = {
-        encoding: null,
-        method: xRequest._method,
-        uri: xRequest._url,
-        body: xRequest._postData,
-        headers: xRequest._headers
-    }
-    const cookies = await page.cookies();
-    options.headers.Cookie = cookies.map(ck => `${ck.name}=${ck.value}`).join(";");
-
-    const response = await request(options);
-    const json_content = JSON.parse(response.toString());
-    const csv = objToCsv(json_content)
-    await store.setValue('14646.csv', csv, {contentType: 'text/csv'});
-    const results = [];
-    for (const line of json_content) {
-        const rate = line.RATE.split(' ');
-        const term = parseInt(line.MINIMUM_TERM.replace(' Month(s)', ''));
-        const cancFee = line.CANCELLATION_FEE ? line.CANCELLATION_FEE.match(/\$([0-9]+\.?[0-9]*)/) : null;
-        let result = {
-            "Date": (new Date()).toLocaleDateString("ISO"),
-            "State": "NY",
-            "Utility": line.UTILITY,
-            "Supplier": line.ESCO,
-            "Rate Type": line.OFFER_TYPE,
-            "Commodity": line.COMMODITY === 'ELECTRIC' ? 'Power' : 'Gas',
-            "Rate Category": null,
-            "Rate": parseFloat(rate[0]),
-            "Rate Unit": rate[1],
-            "Term": term,
-            "Cancellation Fee": cancFee ? parseInt(cancFee[1]) : null,
-            "Renewable Blend": Number(line.PERCENTAGE_RENEWABLE) * 100,
-            "Offer Notes": line.COMMENTS,
-            "Additional Products & Services": null,
-            "Fee": null,
-            "Fee Type": line.CANCELLATION_FEE,
-            "Fee Notes": null,
-            "Termination Notes": line.CANCELLATION_FEE,
-            "Other": line.LOAD_ZONE,
-            // "Csv copy": await store.getPublicUrl('14646.csv')
-        }
-
-        results.push(result);
-    }
-
-    await dataset.pushData(results);
-    await Apify.pushData(results);
-    console.log('Closing Puppeteer...');
-    await browser.close();
-
-    console.log('Done.');
+    log.info('Starting the crawl.');
+    await crawler.run();
+    log.info('Crawl finished.');
 });
-
-function objToCsv(obj) {
-    const replacer = (key, value) => value === null ? '' : value
-    const header = Object.keys(obj[0])
-    let csv = obj.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
-    csv.unshift(header.join(','))
-    csv = csv.join('\r\n');
-    return csv
-}
---
+++ src/routes.js
@@ -0,0 +1,138 @@
+const Apify = require('apify');
+
+const { utils: { log } } = Apify;
+
+const { LABEL, SELECTOR } = require("./config");
+
+const request_promise = require('request-promise');
+
+const csv = require("csv-string");
+
+const fs = require("fs");
+
+const csvToJson = (csv) => {
+    const header = csv[0];
+    const data = csv.slice(1);
+    var csvJSON = [];
+    data.map(line => {
+        var lineJSON = {};
+        line.map((cval, cid) => {
+            lineJSON[header[cid]] = cval;
+        });
+        csvJSON.push(lineJSON);
+    });
+    return csvJSON;
+}
+
+exports.handleBase = async ({ request, page }, requestQueue) => {
+    // open js links
+    await page.hover(SELECTOR.HOVER_TYPE_FIRST);
+    await page.$$eval(SELECTOR.MORE_OPENER, (el => el.map(e => e.click())));
+    await page.hover(SELECTOR.HOVER_TYPE_SECOND);
+    await page.$$eval(SELECTOR.MORE_OPENER, (el => el.map(e => e.click())));
+    //await page.hover(SELECTOR.HOVER_TYPE_FIRST);
+    //const links = await page.$$eval(SELECTOR.UTILITIES, (els => els.map(e => e.href)));
+    await Apify.utils.enqueueLinks({
+        page,
+        requestQueue,
+        selector: SELECTOR.UTILITIES,
+        transformRequestFunction: request => {
+            request.userData.label = LABEL.UTILITY;
+            return request;
+        }
+    });
+};
+
+exports.handleUtility = async ({ request, page }) => {
+    const dataset = await Apify.openDataset('powermatrix');
+
+    await page.waitForSelector(SELECTOR.EXPORT_CSV);
+
+    const CustomerType = await page.$eval("#ctl00_ContentPlaceHolder1_upOffers > div.search-resultbreadcrumb > strong", e => e.innerText.match(/.?\:(.*)/)[1].trim());
+    const utilityName = await page.$eval("div.main-container div.main-left h3", e => e.innerText.trim());
+    let PTCInfo = await page.$eval("div.main-container > div.main-left", e => e.innerText.trim().replace(/\n/g, "").match(/(.?)“Price to Compare” for the generation(.*)\/kWh.?\.\s+/)[0].trim());
+    const PTCTerm = PTCInfo.match(/(.*)period of(.*)is/)[2].trim();
+    const PTCRate = PTCInfo.match(/\.\d+/)[0].trim();
+
+    await page.setRequestInterception(true);
+    await page.click(SELECTOR.EXPORT_CSV);
+    const xRequest = await new Promise(resolve => {
+        page.on('request', interceptedRequest => {
+            interceptedRequest.abort();
+            resolve(interceptedRequest);
+        });
+    });
+
+    const options = {
+        encoding: null,
+        method: xRequest._method,
+        uri: xRequest._url,
+        body: xRequest._postData,
+        headers: xRequest._headers
+    }
+
+    /* add the cookies */
+    const cookies = await page.cookies();
+    options.headers.Cookie = cookies.map(ck => ck.name + '=' + ck.value).join(';');
+
+    /* resend the request */
+    const response = await request_promise(options);
+    // Header
+    //SupplierCompanyName,CompanyName,SupplierAddress,SupplierAddress2,SupplierCity,SupplierState,SupplierZip,SupplierPhone,SupplierWebSiteUrl,TermsOfServiceURL,SignUpNowURL,Price,RateType,Renewable,IsIntroductoryOffer,IntroductoryOfferDetails,TermLength,EarlyTerminationFee,MonthlyFee,IsPromotionalOffer,PromotionalOfferDetails,OfferDetails
+    /*[
+  0 'SupplierCompanyName',
+  1 'CompanyName',
+  2 'SupplierAddress',
+  3 'SupplierAddress2',
+  4 'SupplierCity',
+  5 'SupplierState',
+  6 'SupplierZip',
+  7 'SupplierPhone',
+  8 'SupplierWebSiteUrl',
+  9 'SignUpNowURL',
+  10'Price',
+  11'RateType',
+  12'Renewable',
+  13'IsIntroductoryOffer',
+  14'IntroductoryOfferDetails',
+  15'TermLength',
+  16'EarlyTerminationFee',
+  17'MonthlyFee',
+  18'IsPromotionalOffer',
+  19'PromotionalOfferDetails',
+  20'OfferDetails'
+]*/
+    let response_content = response.toString();
+    let csv_content = csv.parse(response_content);
+    //fs.writeFileSync("test.csv", response_content, err => console.log(err));
+    csvJSON = csvToJson(csv_content);
+
+    for (line of csvJSON) {
+        // skip header
+        let FeeType;
+        if (line.MonthlyFee != 0) { FeeType = "Monthly" } else FeeType = "";
+        let result = {
+            "Date": (new Date()).toLocaleDateString("ISO"),
+            "Commodity": "Power",
+            "State": "OH",
+            "Customer Type": CustomerType,
+            "Utility": utilityName,
+            "Supplier": line.CompanyName,
+            "Rate Type": line.RateType,
+            "Rate": line.Price,
+            "PTC Rate": PTCRate,
+            "Term": line.TermLength,
+            "PTC Term": PTCTerm,
+            "Cancellation Fee": line.EarlyTerminationFee,
+            "Offer Notes": line.OfferDetails.trim(),
+            "Fee": line.MonthlyFee,
+            "Fee Type": FeeType,
+            "Introductory Offer": line.IntroductoryOfferDetails.trim(),
+            "Promotional Offer": line.PromotionalOfferDetails.trim()
+        }
+        //console.log(result);
+        await dataset.pushData(result);
+        await Apify.pushData(result);
+    }
+};
+

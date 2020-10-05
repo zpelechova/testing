c2a4693bf41d6ae365f45e4c74fb220b6d67fc76
Zuzana Pelechová <zpelechova@gmail.com>
2020-10-05 10:01:20 +0200

    oh

--- src/routes.js
+++ src/routes.js
@@ -125,7 +125,7 @@ exports.handleUtility = async ({ request, page}, PTCData ) => {
 
         if (!found) PTCData.push(pagePTCObject);
 
-        console.log(pagePTCObject);
+        console.log(PTCData);
 
         await Apify.setValue('ptc', PTCData);
 

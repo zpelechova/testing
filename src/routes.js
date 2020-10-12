0c974c6f0d86f1c99dc8e88e02d23f3fd5da6305
Zuzana Pelechová <zpelechova@gmail.com>
2020-10-12 14:43:32 +0200

    pa

--- src/routes.js
+++ src/routes.js
@@ -2,132 +2,82 @@ const Apify = require('apify');
 
 const { utils: { log } } = Apify;
 
-const { LABEL, SELECTOR } = require("./config");
-
-const request_promise = require('request-promise');
-
-const csv = require("csv-string");
-
-const fs = require("fs");
-
-const csvToJson = (csv) => {
-    const header = csv[0];
-    const data = csv.slice(1);
-    var csvJSON = [];
-    data.map(line => {
-        var lineJSON = {};
-        line.map((cval, cid) => {
-            lineJSON[header[cid]] = cval;
-        });
-        csvJSON.push(lineJSON);
-    });
-    return csvJSON;
-}
-
-exports.handleBase = async ({ request, page }, requestQueue) => {
-    // open js links
-    await page.hover(SELECTOR.HOVER_TYPE_FIRST);
-    await page.$$eval(SELECTOR.MORE_OPENER, (el => el.map(e => e.click())));
-    await page.hover(SELECTOR.HOVER_TYPE_SECOND);
-    await page.$$eval(SELECTOR.MORE_OPENER, (el => el.map(e => e.click())));
-    //await page.hover(SELECTOR.HOVER_TYPE_FIRST);
-    //const links = await page.$$eval(SELECTOR.UTILITIES, (els => els.map(e => e.href)));
-    await Apify.utils.enqueueLinks({
-        page,
-        requestQueue,
-        selector: SELECTOR.UTILITIES,
-        transformRequestFunction: request => {
-            request.userData.label = LABEL.UTILITY;
-            return request;
-        }
-    });
-};
-
-
-exports.handleUtility = async ({ request, page}, PTCData) => {
+exports.handleStart = async ({ request, page }, requestQueue, zip, originalUrl) => {
     const dataset = await Apify.openDataset('powermatrix');
-    await page.waitForSelector(SELECTOR.EXPORT_CSV);
-
-    const CustomerType = await page.$eval("#ctl00_ContentPlaceHolder1_upOffers > div.search-resultbreadcrumb > strong", e => e.innerText.match(/.?\:(.*)/)[1].trim());
-    const utilityName = await page.$eval("div.main-container div.main-left h3", e => e.innerText.trim());
-    let PTCInfo = await page.$eval("div.main-container > div.main-left", e => e.innerText.trim().replace(/\n/g, "").match(/(.?)rate is\s+\$\d\.\d+(.*)\d+/)[0].trim());
-    const PTCTerm = PTCInfo.match(/(.*)\-\sEffective(.*)\,/)[2].trim();
-    const PTCRate = PTCInfo.match(/\d\.\d+/)[0].trim();
-    const PTCUnit = PTCInfo.match(/per\s[a-zA-Z]+/)[0].trim();
-
-    await page.setRequestInterception(true);
-    await page.click(SELECTOR.EXPORT_CSV);
-    const xRequest = await new Promise(resolve => {
-        page.on('request', interceptedRequest => {
-            interceptedRequest.abort();
-            resolve(interceptedRequest);
+    await page.waitForSelector("footer.l-footer");
+    const results = []
+    if (originalUrl != undefined)
+    {const distributors = await page.$$eval("a.multi-distributor-home", elems => elems.map(elem => elem.getAttribute("data-id")));
+    console.log(originalUrl);
+    let currentUrl = originalUrl.replace("{zip}", zip);
+    for (let distributor of distributors) {
+        await requestQueue.addRequest({
+            url: `${currentUrl}&distributor=${distributor}`
         });
-    });
-
-    const options = {
-        encoding: null,
-        method: xRequest._method,
-        uri: xRequest._url,
-        body: xRequest._postData,
-        headers: xRequest._headers
-    }
-
-    /* add the cookies */
-    const cookies = await page.cookies();
-    options.headers.Cookie = cookies.map(ck => ck.name + '=' + ck.value).join(';');
-
-    /* resend the request */
-    const response = await request_promise(options);
-    
-    let response_content = response.toString();
-    let csv_content = csv.parse(response_content);
-    //fs.writeFileSync("test.csv", response_content, err => console.log(err));
-    csvJSON = csvToJson(csv_content);
-
-    for (line of csvJSON) {
-        // skip header
-        let FeeType;
-        if (line.MonthlyFee != 0) { FeeType = "Monthly" } else FeeType = "";
-        let result = {
+    }}
+    const tables = await page.$$("div.rate-item");
+    for (let tid in tables) {
+        let table = tables[tid];
+        if (await table.$eval("div.supplier-name.enrollmentNo > span.name", el => el.innerText) == "%distributor%") continue;
+        let Supplier = await table.$eval("div.supplier-name.enrollmentNo > span.name", el => el.innerText);
+        let RateType = await table.$eval("div.copy > div.left > span", el => el.innerText);
+        let Term = await table.$eval("div.middle > span.term-length", el => el.innerText);
+        let Rate = await table.$eval("div.supplier-rate > span.rate", el => el.innerText);
+        let CancellationFee = await table.$eval("div.middle > span.cancellation", el => el.innerText);
+        let RenewableBlend = await table.$eval("div.left > span.renewable", el => el.innerText);
+        let Fee = await table.$eval("div.middle > span.enrollment-fee", el => el.innerText);
+        let FeeType = await table.$eval("div.middle > span.monthly-fee", el => el.innerText);
+        let Utility = await page.$eval("div.ratetype-result > div.distributor-name > span.name", el => el.innerText);
+        let PTCRate = await page.$eval("div.distributor-rate > span.rate", el => el.innerText);
+        let PTCName = await page.$eval("div.distributor-name > span.name", el => el.innerText);
+        let Additional = await table.$eval("div.more-info", el => el.innerText);
+        let CustomerType;
+        if (request.loadedUrl.indexOf("shop-for-your-home") != -1) CustomerType = "Residential";
+        else CustomerType = "Small business";
+        results.push({
             "Date": (new Date()).toLocaleDateString("ISO"),
-            "Commodity": "Gas",
-            "State": "OH",
+            "Commodity": "Power",
+            "State": "PA",
             "Customer Type": CustomerType,
-            "Utility": utilityName,
-            "Supplier": line.CompanyName,
+            "Utility": Utility,
+            "Supplier": Supplier.trim(),
             "Rate Category" : "",
-            "Rate Type": line.RateType,
-            "Rate": line.Price,
-            "Term": line.TermLength,
-            "Cancellation Fee": line.EarlyTerminationFee,
-            "Offer Notes": line.OfferDetails,
-            "Fee": line.MonthlyFee,
-            "Fee Notes": FeeType,
-            "Fee Type": line.IntroductoryOfferDetails.trim(),
+            "Rate Type": RateType.match(/.*\:(.*)/)[1].trim(),
+            "Rate": Rate.replace("$", ""),
+            "Term": Term.match(/.*\:(.*)/)[1].trim(),
+            "Cancellation Fee": CancellationFee.match(/.*\:(.*)/)[1].trim(),
+            "Offer Notes": "",
+            "Renewable Blend": RenewableBlend.match(/.*\:(.*)/)[1].replace("%", "").trim(),
+            "Additional Products & Services": Additional.trim().replace(/\n/g, ""),
+            "Fee": Fee.match(/.*\:(.*)/)[1].trim(),
+            "Fee Type": FeeType,
+            "Fee Notes": "",
             "Other Notes": "",
-            "Additional Products & Services": line.PromotionalOfferDetails.trim(),
-            "Rate units": PTCUnit.replace('per ', ''),
-            "Renewable blend": line.Renewable,
-            "Termination Notes": ""
-        }
-        //console.log(result);
-        await dataset.pushData(result);
-        await Apify.pushData(result);
-
+            "Additional Products & Services": "",
+            "Rate units": "$/kWh",
+            "Termination Notes": "",
+            // "zzPTC Rate": PTCRate.replace("$", ""),
+            // "zzPTC Name": PTCName,
+        });
+        
         const pagePTCObject = {
             PTCRate: PTCRate,
-            PTCTerm: PTCTerm,
-            PTCUnit: PTCUnit.replace("per ", ""),
-            utilityName: utilityName,
+            PTCName: PTCName,
             CustomerType: CustomerType,
             FeeType: FeeType
         };
-
-        const found = PTCData.find(e => e.PTCRate === pagePTCObject.PTCRate && e.PTCTerm === pagePTCObject.PTCTerm && e.utilityName === pagePTCObject.utilityName);
-
+    
+        const found = PTCData.find(e => e.PTCRate === pagePTCObject.PTCRate && e.PTCName === pagePTCObject.PTCName);
+    
         if (!found) PTCData.push(pagePTCObject);
-
+    
         await Apify.setValue('ptc', PTCData);
-
     }
+
+    //console.log(results);
+    await dataset.pushData(results);
+    await Apify.pushData(results);
+
+    
+ 
 };

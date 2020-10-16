32b101c165bec4a33c9e4273edcf7f30c74ae1d5
Zuzana Pelechová <zpelechova@gmail.com>
2020-10-16 08:56:41 +0200

    pa

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,9 @@
 
 # First, specify the base Docker image. Apify provides the following base images
 # for your convenience:
-#  apify/actor-node-basic (Node.js 10 on Alpine Linux, small and fast image)
-#  apify/actor-node-chrome (Node.js 10 + Chrome on Debian)
-#  apify/actor-node-chrome-xvfb (Node.js 10 + Chrome + Xvfb on Debian)
+#  apify/actor-node-basic (Node.js on Alpine Linux, small and fast image)
+#  apify/actor-node-chrome (Node.js + Chrome on Debian)
+#  apify/actor-node-chrome-xvfb (Node.js + Chrome + Xvfb on Debian)
 # For more information, see https://apify.com/docs/actor#base-images
 # Note that you can use any other image from Docker Hub.
 FROM apify/actor-node-chrome
@@ -18,7 +18,7 @@ COPY package*.json ./
 # Install NPM packages, skip optional and development dependencies to keep the
 # image small. Avoid logging too much and print the dependency tree for debugging
 RUN npm --quiet set progress=false \
- && npm install --only=prod --no-optional \
+ && npm install --only=prod \
  && echo "Installed NPM packages:" \
  && npm list \
  && echo "Node.js version:" \
@@ -31,5 +31,10 @@ RUN npm --quiet set progress=false \
 # for simple source file changes.
 COPY . ./
 
-# Specify how to run the source code
-CMD npm start
+# Optionally, specify how to launch the source code of your actor.
+# By default, Apify's base Docker images define the CMD instruction
+# that runs the Node.js source code using the command specified
+# in the "scripts.start" section of the package.json file.
+# In short, the instruction looks something like this:
+#
+# CMD npm start
--- src/main.js
+++ src/main.js
@@ -1,168 +1,101 @@
+/**
+ * This template is a production ready boilerplate for developing with `PuppeteerCrawler`.
+ * Use this to bootstrap your projects using the most up-to-date code.
+ * If you're looking for examples or want to learn more, see README.
+ */
+
 const Apify = require('apify');
-const _ = require('underscore');
-const { COUNTRY, LABELS, STARTURLS } = require('./consts');
-const { flat, ExtractItems, findArraysUrl, formatPrice, getProductImage } = require('./tools');
+const { handleStart } = require('./routes');
 
-const stats = {
-    offers: 0,
-};
+const { utils: { log } } = Apify;
 
-const uniqueItems = new Set();
-
-const { log } = Apify.utils;
-
+const PTCData = [];
 
 Apify.main(async () => {
-    const { country = COUNTRY.CZ, debugLog = false, test } = await Apify.getInput();
-    const requestQueue = await Apify.openRequestQueue();
-    const url = country === COUNTRY.CZ ? STARTURLS.CZ : STARTURLS.SK;
-    if (debugLog) {
-        Apify.utils.log.setLevel(Apify.utils.log.LEVELS.DEBUG);
-    }
-    await requestQueue.addRequest({
-        url,
-        userData: {
-            label: LABELS.START,
-        },
-    });
-
-    /*    await requestQueue.addRequest({
-        url: 'https://nakup.itesco.cz/groceries/cs-CZ/shop/ovoce-a-zelenina/ovoce/all',
-        userData: {
-            label: 'PAGINATION',
-        },
-    }); */
-
-    const persistState = async () => {
-        console.log(stats);
+    const { startUrls, zip } = {
+        "startUrls": [
+          {
+            "url": "https://www.papowerswitch.com/shop-for-electricity/shop-for-your-home?type=all&zip={zip}"
+          },
+          {
+            "url": "https://www.papowerswitch.com/shop-for-electricity/shop-for-your-small-business?type=all&zip={zip}"
+          }
+        ],
+        "zip": [
+          "15212",
+          "19601",
+          "19019",
+          "15521",
+          "15237",
+          "17501",
+          "17320"
+        ]
     };
-    Apify.events.on('persistState', persistState);
-
-    const proxyConfiguration = await Apify.createProxyConfiguration({
-        groups: ['CZECH_LUMINATI'],
-    });
-    const crawler = new Apify.CheerioCrawler({
-        requestQueue,
-        maxConcurrency: 10,
-        maxRequestRetries: 5,
-        proxyConfiguration,
-        handlePageTimeoutSecs: 60,
-        requestOptions: {
-            ignoreSslErrors: true,
-        },
-
-        handlePageFunction: async ({ $, request }) => {
-            log.info(`Processing ${request.url}, ${request.userData.label}`);
-            if (request.userData.label === LABELS.START) {
-                const script = $('body').attr('data-redux-state');
-                const urlsCatHtml = JSON.parse(script);
-
-                const startUrls = await findArraysUrl(urlsCatHtml, country);
-                log.debug(`Found ${startUrls.length} on ${request.userData.label}`);
-                for (const item of startUrls) {
-                    await requestQueue.addRequest({
-                        url: item.url,
-                        userData: {
-                            label: LABELS.PAGE,
-                        },
-                    });
+    //const requestList = await Apify.openRequestList('start-urls', startUrls);
+    const requestQueue = await Apify.openRequestQueue();
+    for (let url of startUrls) {
+        if (url.url.indexOf("{zip}") == -1) {
+            throw new Error(`${url.url}: Nowhere to put ZIP code to`);
+        }
+        for (let zipCode of zip) {
+            requestQueue.addRequest({
+                url: url.url.replace("{zip}", zipCode),
+                userData: {
+                    zip: zipCode,
+                    originalUrl: url.url
                 }
-            } else if (request.userData.label === LABELS.PAGE) {
-                try {
-                    if ($('.pagination--page-selector-wrapper ul li').eq(-2)) {
-                        const lastPage = $('.pagination--page-selector-wrapper ul li').eq(-2).text();
-                        const parsedLastPage = parseInt(lastPage);
-                        if (parsedLastPage > 1 && request.url.indexOf('?page=') === -1) {
-                            const pagesArr = _.range(2, parsedLastPage + 1);
-                            for (const page of pagesArr) {
-                                const nextPageUrl = `${request.url}?page=${page}`;
-                                await requestQueue.addRequest({
-                                    url: nextPageUrl,
-                                    userData: {
-                                        label: LABELS.PAGINATION,
-                                    },
-                                });
-                            }
-                        }
-                    }
-                    const requestQueue = await Apify.openRequestQueue();
-                    //add detail pages of all products on the page to requestQueue
-                    const links = $(".product-list").map(function () { return $(this).find('a').attr('href'); }).get();
-                    for (let link of links) {
-                        const absoluteLink = urlClass.resolve(request.url, link);
-                        await requestQueue.addRequest({
-                            url: absoluteLink,
-                            userData: { label: 'DETAIL' },
-                        });
-                        // const items = await ExtractItems($, country, uniqueItems, stats, request);
-                        // log.debug(`Found ${items.length} storing them, ${request.url}`);
-                        // await Apify.pushData(items);
-                    }
-                } catch (e) {
-                    // no items on the page check it out
-                    log.debug(`Check this url, there are no items ${request.url}`);
-                    await Apify.pushData({
-                        status: 'Check this url, there are no items',
-                        url: request.url,
-                    });
-                }
-            } else if (request.userData.label === LABELS.PAGINATION) {
-                try {
-                    // const items = await ExtractItems($, country, uniqueItems, stats, request);
-                    // log.debug(`Found ${items.length} storing them, ${request.url}`);
-                    // await Apify.pushData(items);
-                } catch (e) {
-                    // no items on the page check it out
-                    log.debug(`Check this url, there are no items ${request.url}`);
-                    await Apify.pushData({
-                        status: 'Check this url, there are no items',
-                        url: request.url,
-                    });
-                }
-            }
-        },
-
-        handleFailedRequestFunction: async ({ request }) => {
-            log.error(`Request ${request.url} failed 4 times`);
-        },
-    });
-    await crawler.run();
-    await persistState();
-
-    if (!test) {
-        // calling the keboola upload
-        try {
-            const env = await Apify.getEnv();
-            const run = await Apify.call(
-                'blackfriday/uploader', {
-                    datasetId: env.defaultDatasetId,
-                    upload: true,
-                    actRunId: env.actorRunId,
-                    blackFriday: false,
-                    tableName: country === COUNTRY.CZ ? 'itesco' : 'itesco_sk',
-                }, {
-                    waitSecs: 25,
-                },
-            );
-            console.log(`Keboola upload called: ${run.id}`);
-        } catch (e) {
-            console.log(e);
+            });
         }
 
-        // stats page
-        try {
-            const env = await Apify.getEnv();
-            const run = await Apify.callTask(
-                'blackfriday/status-page-store', {
-                    datasetId: env.defaultDatasetId,
-                    tableName: country === COUNTRY.CZ ? 'itesco' : 'itesco_sk',
-                }, {
-                    waitSecs: 25,
-                },
-            );
-            console.log(`stats upload called: ${run.id}`);
-        } catch (e) {
-            console.log(e);
-        }
     }
+
+    const crawler = new Apify.PuppeteerCrawler({
+        //requestList,
+        requestQueue,
+        useSessionPool: true,
+        persistCookiesPerSession: true,
+        launchPuppeteerOptions: {
+            useApifyProxy: false,
+            // Chrome with stealth should work for most websites.
+            // If it doesn't, feel free to remove this.
+            useChrome: true,
+            stealth: true,
+        },
+
+        handlePageFunction: async (context) => {
+            const { url, userData: { label, zip, originalUrl } } = context.request;
+            log.info('Page opened.', { label, url });
+            await handleStart(context, requestQueue, zip, originalUrl, PTCData);
+        },
+    });
+
+    log.info('Starting the crawl.');
+    await crawler.run();
+
+    for (const ptc of PTCData) {
+        const {PTCRate, PTCName, CustomerType, FeeType} = ptc;
+        await Apify.pushData({
+            "Date": (new Date()).toLocaleDateString("ISO"),
+            "Commodity": "Power",
+            "State": "PA",
+            "Customer Class": CustomerType || "",
+            "Utility": PTCName || "",
+            "Supplier": "",
+            "Rate Category": "",
+            "Rate Type": "PTC",
+            "Rate": PTCRate || "",
+            "Term": "",
+            "Cancellation Fee": "",
+            "Offer Notes": "",
+            "Fee": "",
+            "Fee Notes": FeeType || "",
+            "Fee Type": "",
+            "Other Notes": "",
+            "Additional Products & Services": "",
+            "Rate units": "$/kWh",
+            "Renewable blend": "",
+            "Termination Notes": ""
+        })};
+
+    log.info('Crawl finished.');
 });
